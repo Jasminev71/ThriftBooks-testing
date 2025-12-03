ec43b6efe316a21dfdc72c6dfccce689e2e4b49f
jasminev71 <jasminevillarreal71@gmail.com>
2025-12-03 11:12:56 -0700

    pageobjects set up added to Book details page

--- test/pageobjects/bookDetails.page.js
+++ test/pageobjects/bookDetails.page.js
@@ -1,8 +1,38 @@
-import { $, $$, expect } from '@wdio/globals';
+import { $ } from '@wdio/globals';
 
-class resultsPage extends Page {
-    
+class BookDetails {
+    get formatHardcover() { return $("//button[@type='button'])[10]")}
+    get formatPaperBack() { return $("//button[@class='NewButton WorkSelector-button is-selected']")}
+    get formatMassMarket() { return $("(//button[@type='button'])[12]")}
+    get formatAudioCd() { return $("(//button[@type='button'])[13]")}
+    get checkOutBtn() { return $("//div[@class='AddToCartButton-Desktop Button-container is-small']")}
 
+
+ async defaultHardcover() {
+    await this.formatHardcover.waitForDisplayed();
+ }
+
+ async selectHardcover() {
+    await this.formatHardcover.waitForDisplayed();
+    await this.formatHardcover.click();
+ }
+ async selectPaperback() {
+    await this.formatPaperBack.waitForDisplayed();
+    await this.formatPaperBack.click();
+ }
+ 
+ async selectMassMarket() {
+    await this.formatMassMarket.waitForDisplayed();
+    await this.formatMassMarket.click();
+ }
+ async selectAudioCd() {
+    await this.formatAudioCd.waitForDisplayed();
+    await this.formatAudioCd.click();
+ }
+ async Checkout() {
+    await this.checkOutBtn.waitForDisplayed()
+    await this.checkoutBtn.click();
+ }
     }
 
 
