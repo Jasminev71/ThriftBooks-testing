f1fd8fd5fdebb71876d5258db1f62c970fa95eba
jasminev71 <jasminevillarreal71@gmail.com>
2025-12-03 11:55:35 -0700

    Page Object: Results Page for Selected section testing

--- test/pageobjects/results.page.js
+++ test/pageobjects/results.page.js
@@ -1,16 +1,35 @@
 import { $, $$, expect } from '@wdio/globals';
-import Page from './page'
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
-class resultsPage  {
-    /**
-     * define selectors using getter methods
-     */
-    get flashAlert () {
-        return $('#flash');
+
+
+class ResultsPage  {
+    get selectedDisplay() { return $('//div[.//h3[normalize-space()="Selected"]]')}
+    get clearbtn() { return this.selectedDisplay.$('//a[contains(.,"Clear")]'); }
+    get resultHeader() {return $('//*[contains(text(),"results for")]')}
+    get booksTag() {return $()}
+    get formatTag() {return $()}
+    get conditionTag() {return $()}
+
+ async filterTagsVisible() {
+    //this will ensure that the selcted tags are visable
+ }
+ async addFilterTags () {
+//this will add filter tags and be visabele from slected section
+ }
+
+ async clearAll() {
+    
+  await this.clearBtn.waitForClickable();
+  await this.clearBtn.click();
+  await expect(this.tag("Show Out Of Stock Items")).not.toBeDisplayed()
+ }
+ async clearOneFilter() {
+//this will clear only one filter tag using the "x" icon from corner of the filter tag
+ }
+
+
+
     }
-}
+
 
 export default new ResultsPage(); 
