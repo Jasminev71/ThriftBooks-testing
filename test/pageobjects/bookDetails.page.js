da820fc195bef86bba84fd14fc4f5d8ce7de6340
jasminev71 <jasminevillarreal71@gmail.com>
2025-12-05 21:14:53 -0700

    Selectors changed to stable ones and test ready

--- test/pageobjects/bookDetails.page.js
+++ test/pageobjects/bookDetails.page.js
@@ -2,33 +2,41 @@ import { $ } from '@wdio/globals';
 import Page from './page.js';
 
 class BookDetails extends Page {
-    get formatHardcover() { return $("//button[@type='button'])[10]")}
-    get formatPaperBack() { return $("//button[@class='NewButton WorkSelector-button is-selected']")}
-    get formatMassMarket() { return $("(//button[@type='button'])[12]")}
-    get formatAudioCd() { return $("(//button[@type='button'])[13]")}
-    get checkOutBtn() { return $("//div[@class='AddToCartButton-Desktop Button-container is-small']")}
+    get formatHardcover() { return $("//button[contains(@class,'WorkSelector-button')][.//div[normalize-space()='Hardcover']]") }
+    get formatPaperBack() { return $("//button[contains(@class,'WorkSelector-button')][.//div[normalize-space()='Paperback']]")}
+    get formatMassMarket() { return $("//button[contains(@class,'WorkSelector-button')][.//div[normalize-space()='Mass Market Paperback']]") }
+    get formatDigital() { return $("//button[contains(@class,'WorkSelector-button')][.//div[normalize-space()='Digital']]")}
+   
 
+
+
+
+   async open() {
+      return super.open('/w/oathbringer_brandon-sanderson/13873541/item/#edition=13683159&idiq=24662077');
+   }
 
  async defaultHardcover() {
     await this.formatHardcover.waitForDisplayed();
+    await this.displayHardCover.waitForDisplayed()
  }
 
  async selectHardcover() {
-    await this.formatHardcover.waitForDisplayed();
+    await this.formatHardcover.waitForClickable();
     await this.formatHardcover.click();
  }
  async selectPaperback() {
-    await this.formatPaperBack.waitForDisplayed();
+    await this.formatPaperBack.waitForClickable();
     await this.formatPaperBack.click();
+   
  }
  
  async selectMassMarket() {
-    await this.formatMassMarket.waitForDisplayed();
+    await this.formatMassMarket.waitForClickable();
     await this.formatMassMarket.click();
  }
- async selectAudioCd() {
-    await this.formatAudioCd.waitForDisplayed();
-    await this.formatAudioCd.click();
+ async selectDigital() {
+    await this.formatDigital.waitForClickable();
+    await this.formatDigital.click();
  }
  async Checkout() {
     await this.checkOutBtn.waitForDisplayed()
