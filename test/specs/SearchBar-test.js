4e203bb93e8659f529f3886bc54089389684c396
jasminev71 <jasminevillarreal71@gmail.com>
2025-12-08 10:23:17 -0700

    Ready for First revision 1.0

--- test/specs/SearchBar-test.js
+++ test/specs/SearchBar-test.js
@@ -7,48 +7,48 @@ describe('SearchBar Tesing', () => {
       
      });
         it('Search by book title using enter key', async () => {
-            await SearchPage.basicSearchKey();
+            await SearchPage.basicSearchKey('MistBorn');
             
         })
 
-        it('Seach by book title using Search Button', async () => {
-            await SearchPage.basicSearchBtn();
+        it('Basic Search', async () => {
+            await SearchPage.basicSearchBtn('Dracula');
         })
         
         it('Search By Author Name', async () => {
-            await SearchPage.authorName();
+            await SearchPage.authorName('Brandon Sanderson');
            
         })
         
         it('Search by ISBN', async () => {
-            await SearchPage.isbnLookUp();
+            await SearchPage.isbnLookUp('9780765326355');
         })
         
         it('Invalid query results', async () =>{
-            await SearchPage.invaildSearch() 
+            await SearchPage.invaildSearch('BookTitle123') 
         })
 
         it('Empty Search Query', async () => {
-            await SearchPage.searchSubmit()
+            await SearchPage.searchSubmit('')
         })
 
         it('Search Bar using special Characters', async () => {
-            await SearchPage.specialCharSearch();
+            await SearchPage.specialCharSearch('@@(#&)');
         })
 
         it(' Extra spacing between search query', async () => {
-            await SearchPage.trailingSpaces();
+            await SearchPage.trailingSpaces("  Oathbringer  ");
         })
 
         it('Inconsistent case cap in search bar', async () => {
-            await SearchPage.caseInsensitivity();
+            await SearchPage.caseInsensitivity('OaThBRiNGer');
         })
 
         it('Single Character Search', async () => {
-            await SearchPage.shortSearch();
+            await SearchPage.shortSearch('a');
         })
         it('Long Character Search', async () => {
-            await SearchPage.longSearch();
+            await SearchPage.longSearch('b');
         })
 })
  
