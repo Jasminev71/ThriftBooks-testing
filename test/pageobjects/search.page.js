53baa4ae196b7e91f1a84731cdd9b3c63f61c40c
jasminev71 <jasminevillarreal71@gmail.com>
2025-12-12 09:18:30 -0700

    SearchBar version 1.1 ready for review

--- test/pageobjects/search.page.js
+++ test/pageobjects/search.page.js
@@ -7,10 +7,10 @@ class SearchPage extends HomePage {
  get searchField() { return $('input[placeholder="Search 19 million titles by title, author, or ISBN"]'); }
  get searchButton() { return $('.header-search-submit'); }
  get clearSearchField() { return $('.header-search-reset'); }
-get resultHeader() {  return $('.Search-sortBar-results'); }
-get titleHeader() { return $('h1[itemprop="name"]'); }
+ get resultHeader() {  return $('.Search-sortBar-results'); }
+ get titleHeader() { return $('h1[itemprop="name"]'); }
 
-async runSearchtests() {
+async runSearchTests() {
    const search = ['Mistborn', 'Dracula', 'BookTitle123', 'a', 'b'.repeat(250), 'Brandon Sanderson','!)#$)@', '  Way of kings  ', 'OaTHbRinger'];
       for (const book of search ) {
             await this.searchField.waitForDisplayed();
@@ -29,13 +29,13 @@ async runSearchtests() {
       
       }
 
-async isbnSearch() {
+async isbnSearch(isbn) {
    await this.searchField.waitForDisplayed();
-   await this.searchField.setValue('014143984X')
+   await this.searchField.setValue(isbn)
    await this.searchButton.click();
    await expect(this.titleHeader).toBeDisplayed();
    
-   await this.searchField.setValue('014143984X');
+   await this.searchField.setValue(isbn);
    await this.clearSearchField.click();
    await this.searchField.waitForDisplayed()
    }
--- test/specs/SearchBar-test.js
+++ test/specs/SearchBar-test.js
@@ -1,55 +1,16 @@
-import SearchPage from '../pageobjects/search.page.js';
+import SearchPage from '../pageobjects/search.page';
 
 
 describe('SearchBar Tesing', () => {
    beforeEach(async () => { 
       await SearchPage.open();
-      
-     });
-        it('Search by book title using enter key', async () => {
-            await SearchPage.basicSearchKey('MistBorn');
-            
-        })
-
-        it('Basic Search', async () => {
-            await SearchPage.basicSearchBtn('Dracula');
-        })
-        
-        it('Search By Author Name', async () => {
-            await SearchPage.authorName('Brandon Sanderson');
-           
-        })
-        
-        it('Search by ISBN', async () => {
-            await SearchPage.isbnLookUp('9780765326355');
-        })
-        
-        it('Invalid query results', async () =>{
-            await SearchPage.invaildSearch('BookTitle123') 
-        })
-
-        it('Empty Search Query', async () => {
-            await SearchPage.searchSubmit('')
-        })
-
-        it('Search Bar using special Characters', async () => {
-            await SearchPage.specialCharSearch('@@(#&)');
-        })
-
-        it(' Extra spacing between search query', async () => {
-            await SearchPage.trailingSpaces("  Oathbringer  ");
-        })
-
-        it('Inconsistent case cap in search bar', async () => {
-            await SearchPage.caseInsensitivity('OaThBRiNGer');
-        })
-
-        it('Single Character Search', async () => {
-            await SearchPage.shortSearch('a');
-        })
-        it('Long Character Search', async () => {
-            await SearchPage.longSearch('b');
-        })
+     })
+       it('Search with Search Button', async () => {
+        await SearchPage.runSearchTests()
+       })
+      it('Search using ISBN', async () => {
+         await SearchPage.isbnSearch('014143984X')
+      })
 })
  
 
